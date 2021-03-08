b78c049a850dd3ca3c03b86ec0498239e7af2e62
Baldinini <baldinat063@gmail.com>
2021-03-08 21:26:53 +0200

    created README.md

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-"# university-service" 
+# university-service
+
+**Running the Project**
+
+1. Download and install the [JDK]( https://www.oracle.com/ru/java/technologies/javase-downloads.html).
+2. Download and install [MySQL Server](https://dev.mysql.com/downloads/).
+3. Find file _db.properties_ in resources and change the parameters to yours:
+    + url: jdbc:mysql://"host name":"port"/"name db"?serverTimezone=UTC
+    + username: "username"
+    + password: "password"
+4. Run the project. 
+
+**The app has these commands:**
+
+1. Who is head of department;
+2. Show department statistics;
+3. Show the average salary for the department;
+4. Show count of employee for the department;
+5. Global search by a template.
+
+Enter 'exit' - for closing the program.
--- src/main/java/com/example/universityservice/UniversityServiceApplication.java
+++ src/main/java/com/example/universityservice/UniversityServiceApplication.java
@@ -2,13 +2,6 @@ package com.example.universityservice;
 
 import com.example.universityservice.console.Application;
 import com.example.universityservice.console.CommandHandler;
-import com.example.universityservice.model.Degree;
-import com.example.universityservice.model.Department;
-import com.example.universityservice.model.Lector;
-import com.example.universityservice.service.DepartmentService;
-import com.example.universityservice.service.LectorService;
-import java.util.Map;
-import java.util.Set;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -17,49 +10,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class UniversityServiceApplication {
 
     public static void main(String[] args) {
-        Lector assistance = new Lector();
-        assistance.setName("Nina");
-        assistance.setLastname("Lol");
-        assistance.setSalary(12000);
-        assistance.setDegree(Degree.ASSISTANT);
-
-        Lector assProfessor = new Lector();
-        assProfessor.setName("bob");
-        assProfessor.setLastname("Lol");
-        assProfessor.setSalary(15000);
-        assProfessor.setDegree(Degree.ASSOCIATE_PROFESSOR);
-
-        Lector professor = new Lector();
-        professor.setName("Rick");
-        professor.setLastname("Lol");
-        professor.setSalary(20000);
-        professor.setDegree(Degree.PROFESSOR);
-
         ConfigurableApplicationContext context =
                 SpringApplication.run(UniversityServiceApplication.class, args);
 
-        LectorService lectorService = context.getBean(LectorService.class);
-        lectorService.save(assistance);
-        lectorService.save(assProfessor);
-        lectorService.save(professor);
-
-        Department aviaDepartment = new Department();
-        aviaDepartment.setName("Avia");
-        aviaDepartment.setHeadOfDepartment(professor);
-        aviaDepartment.setLectors(Set.of(assistance, assProfessor, professor));
-
-        Department carDepartment = new Department();
-        carDepartment.setName("Car");
-        carDepartment.setHeadOfDepartment(assistance);
-        carDepartment.setLectors(Set.of(assistance, professor));
-
-        DepartmentService departmentService = context.getBean(DepartmentService.class);
-        departmentService.save(aviaDepartment);
-        departmentService.save(carDepartment);
-
-        Map<String, CommandHandler> beansOfType = context.getBeansOfType(CommandHandler.class);
-
-        Application application = new Application(beansOfType);
+        Application application = new Application(context.getBeansOfType(CommandHandler.class));
         application.run();
     }
 }
